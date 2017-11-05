7bd7667b6ad28e306e128c77c0c0f21fd74ac9b8
Casey Gruppioni <casey@appcues.com>
2017-11-04 22:31:27 -0400

    Add id to media.

--- all-queries.sql
+++ all-queries.sql
@@ -19,21 +19,22 @@ INSERT INTO users VALUES (88884083,'ddlovato','Demi Lovato','https://instagram.f
 INSERT INTO users VALUES (1,'badgalriri','Rihanna','https://instagram.fbed1-1.fna.fbcdn.net/t51.2885-19/11032926_1049846535031474_260957621_a.jpg',' @FentyBeauty is out now!! ri-hanna.io/fentybeauty','http://fenty.com',4119,57800000,1271);
 
 CREATE TABLE media (
+    `id` INT,
     `type` VARCHAR(5) CHARACTER SET utf8,
     `user_id` INT,
     `filter` VARCHAR(6) CHARACTER SET utf8,
     `created_at` INT,
     `link` VARCHAR(64) CHARACTER SET utf8
 );
-INSERT INTO media VALUES ('image',1533383,'Walden',1509838842,'https://www.instagram.com/p/BaXfVSCnvGT/?taken-by=colesprouse');
-INSERT INTO media VALUES ('image',1533383,NULL,1509838842,'https://www.instagram.com/p/BaM0R6dHQvj/?taken-by=colesprouse');
-INSERT INTO media VALUES ('image',1,'Walden',1509818147,'https://www.instagram.com/p/Ba7G9dnDIbs/?taken-by=badgalriri');
-INSERT INTO media VALUES ('image',1,'Kelvin',1509568726,'https://www.instagram.com/p/BaeV3hojH1q/?taken-by=badgalriri');
-INSERT INTO media VALUES ('image',88884083,'1977',1509806869,'https://www.instagram.com/p/BbCSkYyF6HV/?taken-by=ddlovato');
-INSERT INTO media VALUES ('image',88884083,'Walden',1509832230,'https://www.instagram.com/p/Ba8e-iIlUsl/?taken-by=ddlovato');
-INSERT INTO media VALUES ('video',221183,NULL,1509838592,'https://www.instagram.com/p/BbEDFmPBxX7/?taken-by=iamcardib');
-INSERT INTO media VALUES ('video',1574083,NULL,1509838258,'https://www.instagram.com/p/BbFryuaDVzc/?taken-by=snoopdogg');
-INSERT INTO media VALUES ('video',111122283,'Walden',1509627234,'https://www.instagram.com/p/BaPogfqnjik/?taken-by=tiffanyhaddish');
+INSERT INTO media VALUES (1123,'image',1533383,'Walden',1509838842,'https://www.instagram.com/p/BaXfVSCnvGT/?taken-by=colesprouse');
+INSERT INTO media VALUES (43533,'image',1533383,NULL,1509838842,'https://www.instagram.com/p/BaM0R6dHQvj/?taken-by=colesprouse');
+INSERT INTO media VALUES (56234,'image',1,'Walden',1509818147,'https://www.instagram.com/p/Ba7G9dnDIbs/?taken-by=badgalriri');
+INSERT INTO media VALUES (987923,'image',1,'Kelvin',1509568726,'https://www.instagram.com/p/BaeV3hojH1q/?taken-by=badgalriri');
+INSERT INTO media VALUES (23429837,'image',88884083,'1977',1509806869,'https://www.instagram.com/p/BbCSkYyF6HV/?taken-by=ddlovato');
+INSERT INTO media VALUES (2349872398,'image',88884083,'Walden',1509832230,'https://www.instagram.com/p/Ba8e-iIlUsl/?taken-by=ddlovato');
+INSERT INTO media VALUES (234234,'video',221183,NULL,1509838592,'https://www.instagram.com/p/BbEDFmPBxX7/?taken-by=iamcardib');
+INSERT INTO media VALUES (7777,'video',1574083,NULL,1509838258,'https://www.instagram.com/p/BbFryuaDVzc/?taken-by=snoopdogg');
+INSERT INTO media VALUES (98989,'video',111122283,'Walden',1509627234,'https://www.instagram.com/p/BaPogfqnjik/?taken-by=tiffanyhaddish');
 
 CREATE TABLE likes (
     `user_id` INT,
--- media.sql
+++ media.sql
@@ -1,16 +1,17 @@
 CREATE TABLE media (
+    `list_id` INT,
     `list_type` VARCHAR(5) CHARACTER SET utf8,
     `list_user_id` INT,
     `list_filter` VARCHAR(6) CHARACTER SET utf8,
     `list_created_at` INT,
     `list_link` VARCHAR(64) CHARACTER SET utf8
 );
-INSERT INTO media VALUES ('image',1533383,'Walden',1509838842,'https://www.instagram.com/p/BaXfVSCnvGT/?taken-by=colesprouse');
-INSERT INTO media VALUES ('image',1533383,NULL,1509838842,'https://www.instagram.com/p/BaM0R6dHQvj/?taken-by=colesprouse');
-INSERT INTO media VALUES ('image',1,'Walden',1509818147,'https://www.instagram.com/p/Ba7G9dnDIbs/?taken-by=badgalriri');
-INSERT INTO media VALUES ('image',1,'Kelvin',1509568726,'https://www.instagram.com/p/BaeV3hojH1q/?taken-by=badgalriri');
-INSERT INTO media VALUES ('image',88884083,'1977',1509806869,'https://www.instagram.com/p/BbCSkYyF6HV/?taken-by=ddlovato');
-INSERT INTO media VALUES ('image',88884083,'Walden',1509832230,'https://www.instagram.com/p/Ba8e-iIlUsl/?taken-by=ddlovato');
-INSERT INTO media VALUES ('video',221183,NULL,1509838592,'https://www.instagram.com/p/BbEDFmPBxX7/?taken-by=iamcardib');
-INSERT INTO media VALUES ('video',1574083,NULL,1509838258,'https://www.instagram.com/p/BbFryuaDVzc/?taken-by=snoopdogg');
-INSERT INTO media VALUES ('video',111122283,'Walden',1509627234,'https://www.instagram.com/p/BaPogfqnjik/?taken-by=tiffanyhaddish');
+INSERT INTO media VALUES (1123,'image',1533383,'Walden',1509838842,'https://www.instagram.com/p/BaXfVSCnvGT/?taken-by=colesprouse');
+INSERT INTO media VALUES (43533,'image',1533383,NULL,1509838842,'https://www.instagram.com/p/BaM0R6dHQvj/?taken-by=colesprouse');
+INSERT INTO media VALUES (56234,'image',1,'Walden',1509818147,'https://www.instagram.com/p/Ba7G9dnDIbs/?taken-by=badgalriri');
+INSERT INTO media VALUES (987923,'image',1,'Kelvin',1509568726,'https://www.instagram.com/p/BaeV3hojH1q/?taken-by=badgalriri');
+INSERT INTO media VALUES (23429837,'image',88884083,'1977',1509806869,'https://www.instagram.com/p/BbCSkYyF6HV/?taken-by=ddlovato');
+INSERT INTO media VALUES (2349872398,'image',88884083,'Walden',1509832230,'https://www.instagram.com/p/Ba8e-iIlUsl/?taken-by=ddlovato');
+INSERT INTO media VALUES (234234,'video',221183,NULL,1509838592,'https://www.instagram.com/p/BbEDFmPBxX7/?taken-by=iamcardib');
+INSERT INTO media VALUES (7777,'video',1574083,NULL,1509838258,'https://www.instagram.com/p/BbFryuaDVzc/?taken-by=snoopdogg');
+INSERT INTO media VALUES (98989,'video',111122283,'Walden',1509627234,'https://www.instagram.com/p/BaPogfqnjik/?taken-by=tiffanyhaddish');
